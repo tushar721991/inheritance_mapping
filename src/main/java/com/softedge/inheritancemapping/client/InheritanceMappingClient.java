cd3691bd46118ee295881722285375d9c92c7c25
unknown <thakurtusharm@gmail.com>
2018-11-24 12:26:47 +0530

    deleted readme file

--- src/main/java/com/softedge/inheritancemapping/client/InheritanceMappingClient.java
+++ src/main/java/com/softedge/inheritancemapping/client/InheritanceMappingClient.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import com.softedge.inheritancemapping.model.CardPayment;
 import com.softedge.inheritancemapping.model.ChequePayment;
 import com.softedge.inheritancemapping.model.Payment;
 import com.softedge.inheritancemapping.util.SessionFactoryProvider;
@@ -16,8 +17,9 @@ public class InheritanceMappingClient {
 		SessionFactory factory = SessionFactoryProvider.getSessionFactory();
 		Session session = factory.openSession();
 		
-		Payment payment = new ChequePayment(101, LocalDate.now(), "Vodafone Bill", 240.0, 12456, "Order");
-		
+//		Payment payment = new ChequePayment(101, LocalDate.now(), "Vodafone Bill", 240.0, 12456, "Order");
+
+		Payment payment = new CardPayment(101, LocalDate.now(), "Internet Bill", 240.0, 1234567812345678l, "VISA", 12, 2020);
 		Transaction tx = session.beginTransaction();
 		
 		session.save(payment);
